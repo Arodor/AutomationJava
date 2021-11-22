3c3570d6066c51fa2875c9250cb749662ac3b320
Arodor <hristobalinov@gmail.com>
2021-11-22 21:29:58 +0100

    Uploading numChecker exercise.

---
+++ JavaExerciesQA/src/main/java/numChecker.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class numChecker {
+    public static void main(String[] args) {
+
+
+        printNums(10,20,33);
+    }
+
+
+    public static void printNums(int a, int b, int c){
+
+        if ((a>b) && (a> c))
+        {
+            System.out.println("The greatest number is "+ a);
+        }
+        if (b>a && b>c)
+        {
+            System.out.println("The greatest number is "+ b);
+
+        }
+        if (c>a && b<c)
+        {
+            System.out.println("The greatest number is "+ c);
+
+        }
+    }
+}
